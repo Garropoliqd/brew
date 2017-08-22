Garropoliqd/brew

commit 53f0d1a5ab2e27a4654e25eef485042128bbc1dd

Simplify CurlDownloadStrategy.

Library/Homebrew/download_strategy.rb
@@ -375,75 +375,59 @@ class CurlDownloadStrategy < AbstractFileDownloadStrategy
       ohai "Downloading from #{url}"
     end
 
-    urls = actual_urls(url)
-    unless urls.empty?
-      ohai "Downloading from #{urls.last}"
-      if !ENV["HOMEBREW_NO_INSECURE_REDIRECT"].nil? && url.start_with?("https://") &&
-         urls.any? { |u| !u.start_with? "https://" }
-        puts "HTTPS to HTTP redirect detected & HOMEBREW_NO_INSECURE_REDIRECT is set."
-        raise CurlDownloadStrategyError, url
-      end
-      url = urls.last
-    end
-
-    curl url, "-C", downloaded_size, "-o", temporary_path
+    curl_download resolved_url(url), to: temporary_path
   end
 
   # Curl options to be always passed to curl,
-  # with raw head calls (`curl -I`) or with actual `fetch`.
+  # with raw head calls (`curl --head`) or with actual `fetch`.
   def _curl_opts
-    copts = []
-    copts << "--user" << meta.fetch(:user) if meta.key?(:user)
-    copts
+    return ["--user" << meta.fetch(:user)] if meta.key?(:user)
+    []
   end
 
-  def actual_urls(url)
-    urls = []
-    curl_args = _curl_opts << "-I" << "-L" << url
-    Utils.popen_read("curl", *curl_args).scan(/^Location: (.+)$/).map do |m|
-      urls << URI.join(urls.last || url, m.first.chomp).to_s
+  def resolved_url(url)
+    redirect_url, _, status = curl_output(
+      *_curl_opts, "--silent", "--head",
+      "--write-out", "%{redirect_url}",
+      "--output", "/dev/null",
+      url.to_s
+    )
+
+    return url unless status.success?
+    return url if redirect_url.empty?
+
+    ohai "Downloading from #{redirect_url}"
+    if ENV["HOMEBREW_NO_INSECURE_REDIRECT"] &&
+       url.start_with?("https://") && !redirect_url.start_with?("https://")
+      puts "HTTPS to HTTP redirect detected & HOMEBREW_NO_INSECURE_REDIRECT is set."
+      raise CurlDownloadStrategyError, url
     end
-    urls
+
+    redirect_url
   end
 
-  def downloaded_size
-    temporary_path.size? || 0
-  end
-
-  def curl(*args)
+  def curl(*args, **options)
     args.concat _curl_opts
     args << "--connect-timeout" << "5" unless mirrors.empty?
-    super
+    super(*args, **options)
   end
 end
 
 # Detect and download from Apache Mirror
 class CurlApacheMirrorDownloadStrategy < CurlDownloadStrategy
   def apache_mirrors
-    rd, wr = IO.pipe
-    buf = ""
+    mirrors, = Open3.capture3(
+      *curl_args(*_curl_opts, "--silent", "--location", "#{@url}&asjson=1"),
+    )
 
-    pid = fork do
-      ENV.delete "HOMEBREW_CURL_VERBOSE"
-      rd.close
-      $stdout.reopen(wr)
-      $stderr.reopen(wr)
-      curl "#{@url}&asjson=1"
-    end
-    wr.close
-
-    rd.readline if ARGV.verbose? # Remove Homebrew output
-    buf << rd.read until rd.eof?
-    rd.close
-    Process.wait(pid)
-    buf
+    JSON.parse(mirrors)
   end
 
   def _fetch
     return super if @tried_apache_mirror
     @tried_apache_mirror = true
 
-    mirrors = JSON.parse(apache_mirrors)
+    mirrors = apache_mirrors
     path_info = mirrors.fetch("path_info")
     @url = mirrors.fetch("preferred") + path_info
     @mirrors |= %W[https://archive.apache.org/dist/#{path_info}]
@@ -460,7 +444,7 @@ end
 class CurlPostDownloadStrategy < CurlDownloadStrategy
   def _fetch
     base_url, data = @url.split("?")
-    curl base_url, "-d", data, "-C", downloaded_size, "-o", temporary_path
+    curl_download base_url, "--data", data, to: temporary_path
   end
 end
 
@@ -530,7 +514,7 @@ class S3DownloadStrategy < CurlDownloadStrategy
       s3url = obj.public_url
     end
 
-    curl s3url, "-C", downloaded_size, "-o", temporary_path
+    curl_download s3url, to: temporary_path
   end
 end
 
@@ -566,7 +550,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 
   def _fetch
-    curl download_url, "-C", downloaded_size, "-o", temporary_path
+    curl_download download_url, to: temporary_path
   end
 
   private
@@ -615,7 +599,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
   def _fetch
     # HTTP request header `Accept: application/octet-stream` is required.
     # Without this, the GitHub API will respond with metadata, not binary.
-    curl download_url, "-C", downloaded_size, "-o", temporary_path, "-H", "Accept: application/octet-stream"
+    curl_download download_url, "--header", "Accept: application/octet-stream", to: temporary_path
   end
 
   private
@@ -915,18 +899,27 @@ class GitHubGitDownloadStrategy < GitDownloadStrategy
   def github_last_commit
     return if ENV["HOMEBREW_NO_GITHUB_API"]
 
-    output, _, status = curl_output "-H", "Accept: application/vnd.github.v3.sha", \
-      "-I", "https://api.github.com/repos/#{@user}/#{@repo}/commits/#{@ref}"
+    output, _, status = curl_output(
+      "--silent", "--head", "--location",
+      "-H", "Accept: application/vnd.github.v3.sha",
+      "https://api.github.com/repos/#{@user}/#{@repo}/commits/#{@ref}"
+    )
 
-    commit = output[/^ETag: \"(\h+)\"/, 1] if status.success?
+    return unless status.success?
+
+    commit = output[/^ETag: \"(\h+)\"/, 1]
     version.update_commit(commit) if commit
     commit
   end
 
   def multiple_short_commits_exist?(commit)
     return if ENV["HOMEBREW_NO_GITHUB_API"]
-    output, _, status = curl_output "-H", "Accept: application/vnd.github.v3.sha", \
-      "-I", "https://api.github.com/repos/#{@user}/#{@repo}/commits/#{commit}"
+
+    output, _, status = curl_output(
+      "--silent", "--head", "--location",
+      "-H", "Accept: application/vnd.github.v3.sha",
+      "https://api.github.com/repos/#{@user}/#{@repo}/commits/#{commit}"
+    )
 
     !(status.success? && output && output[/^Status: (200)/, 1] == "200")
   end
@@ -1159,15 +1152,13 @@ class DownloadStrategyDetector
       SubversionDownloadStrategy
     when %r{^cvs://}
       CVSDownloadStrategy
-    when %r{^https?://(.+?\.)?googlecode\.com/hg}
-      MercurialDownloadStrategy
-    when %r{^hg://}
+    when %r{^hg://}, %r{^https?://(.+?\.)?googlecode\.com/hg}
       MercurialDownloadStrategy
     when %r{^bzr://}
       BazaarDownloadStrategy
     when %r{^fossil://}
       FossilDownloadStrategy
-    when %r{^http://svn\.apache\.org/repos/}, %r{^svn\+http://}
+    when %r{^svn\+http://}, %r{^http://svn\.apache\.org/repos/}
       SubversionDownloadStrategy
     when %r{^https?://(.+?\.)?sourceforge\.net/hgweb/}
       MercurialDownloadStrategy

Library/Homebrew/cask/lib/hbc/download_strategy.rb
@@ -90,13 +90,8 @@ module Hbc
       end
     end
 
-    def downloaded_size
-      temporary_path.size? || 0
-    end
-
     def _fetch
-      odebug "Calling curl with args #{cask_curl_args}"
-      curl(*cask_curl_args)
+      curl_download url, *cask_curl_args, to: temporary_path, user_agent: uri_object.user_agent
     end
 
     def fetch
@@ -131,23 +126,7 @@ module Hbc
     private
 
     def cask_curl_args
-      default_curl_args.tap do |args|
-        args.concat(user_agent_args)
-        args.concat(cookies_args)
-        args.concat(referer_args)
-      end
-    end
-
-    def default_curl_args
-      [url, "-C", downloaded_size, "-o", temporary_path]
-    end
-
-    def user_agent_args
-      if uri_object.user_agent
-        ["-A", uri_object.user_agent]
-      else
-        []
-      end
+      cookies_args + referer_args
     end
 
     def cookies_args
@@ -181,8 +160,7 @@ module Hbc
 
   class CurlPostDownloadStrategy < CurlDownloadStrategy
     def cask_curl_args
-      super
-      default_curl_args.concat(post_args)
+      super.concat(post_args)
     end
 
     def post_args

Library/Homebrew/cask/lib/hbc/audit.rb
@@ -143,7 +143,15 @@ module Hbc
 
     def check_appcast_http_code
       odebug "Verifying appcast returns 200 HTTP response code"
-      result = @command.run("/usr/bin/curl", args: ["--compressed", "--location", "--user-agent", URL::FAKE_USER_AGENT, "--output", "/dev/null", "--write-out", "%{http_code}", cask.appcast], print_stderr: false)
+
+      curl_executable, *args = curl_args(
+        "--compressed", "--location", "--fail",
+        "--write-out", "%{http_code}",
+        "--output", "/dev/null",
+        cask.appcast,
+        user_agent: :fake
+      )
+      result = @command.run(curl_executable, args: args, print_stderr: false)
       if result.success?
         http_code = result.stdout.chomp
         add_warning "unexpected HTTP response code retrieving appcast: #{http_code}" unless http_code == "200"

Library/Homebrew/cask/lib/hbc/cask_loader.rb
@@ -71,7 +71,7 @@ module Hbc
 
         begin
           ohai "Downloading #{url}."
-          curl url, "-o", path
+          curl_download url, to: path
         rescue ErrorDuringExecution
           raise CaskUnavailableError.new(token, "Failed to download #{Formatter.url(url)}.")
         end

Library/Homebrew/cask/lib/hbc/dsl/appcast.rb
@@ -12,7 +12,11 @@ module Hbc
       end
 
       def calculate_checkpoint
-        result = SystemCommand.run("/usr/bin/curl", args: ["--compressed", "--location", "--user-agent", URL::FAKE_USER_AGENT, "--fail", @uri], print_stderr: false)
+        curl_executable, *args = curl_args(
+          "--compressed", "--location", "--fail", @uri,
+          user_agent: :fake
+        )
+        result = SystemCommand.run(curl_executable, args: args, print_stderr: false)
 
         checkpoint = if result.success?
           processed_appcast_text = result.stdout.gsub(%r{<pubDate>[^<]*</pubDate>}m, "")

Library/Homebrew/cask/lib/hbc/url.rb
@@ -1,8 +1,6 @@
 module Hbc
   class URL
-    FAKE_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10) https://caskroom.github.io".freeze
-
-    attr_reader :using, :revision, :trust_cert, :uri, :cookies, :referer, :data
+    attr_reader :using, :revision, :trust_cert, :uri, :cookies, :referer, :data, :user_agent
 
     extend Forwardable
     def_delegators :uri, :path, :scheme, :to_s
@@ -17,18 +15,13 @@ module Hbc
 
     def initialize(uri, options = {})
       @uri        = Hbc::UnderscoreSupportingURI.parse(uri)
-      @user_agent = options[:user_agent]
+      @user_agent = options.fetch(:user_agent, :default)
       @cookies    = options[:cookies]
       @referer    = options[:referer]
       @using      = options[:using]
       @revision   = options[:revision]
       @trust_cert = options[:trust_cert]
       @data       = options[:data]
-    end
-
-    def user_agent
-      return FAKE_USER_AGENT if @user_agent == :fake
-      @user_agent
     end
   end
 end

Library/Homebrew/cask/lib/hbc/verify/gpg.rb
@@ -33,7 +33,7 @@ module Hbc
         meta_dir = cached || cask.metadata_subdir("gpg", :now, true)
         sig_path = meta_dir.join("signature.asc")
 
-        curl(cask.gpg.signature, "-o", sig_path.to_s) unless cached || force
+        curl_download cask.gpg.signature, to: sig_path unless cached || force
 
         sig_path
       end

Library/Homebrew/dev-cmd/audit.rb
@@ -242,12 +242,10 @@ class FormulaAuditor
 
   def self.http_content_headers_and_checksum(url, hash_needed: false, user_agent: :default)
     max_time = hash_needed ? "600" : "25"
-    args = curl_args(
-      extra_args: ["--connect-timeout", "15", "--include", "--max-time", max_time, url],
-      show_output: true,
-      user_agent: user_agent,
+    output, = curl_output(
+      "--connect-timeout", "15", "--include", "--max-time", max_time, "--location", url,
+      user_agent: user_agent
     )
-    output = Open3.popen3(*args) { |_, stdout, _, _| stdout.read }
 
     status_code = :unknown
     while status_code == :unknown || status_code.to_s.start_with?("3")

commit 91f6763b60585a71e2f86411db72fcc256d68a6a

Fix curl --user flag being broken in 53f0d1a5

Library/Homebrew/download_strategy.rb
@@ -381,7 +381,7 @@ class CurlDownloadStrategy < AbstractFileDownloadStrategy
   # Curl options to be always passed to curl,
   # with raw head calls (`curl --head`) or with actual `fetch`.
   def _curl_opts
-    return ["--user" << meta.fetch(:user)] if meta.key?(:user)
+    return ["--user", meta.fetch(:user)] if meta.key?(:user)
     []
   end
 