Garropoliqd/brew

commit 4d634c193a7e95b5344204119b9d1e11efff7f45

Move condition to nested `if` statement.

Library/Homebrew/cmd/search.rb
@@ -100,10 +100,10 @@ module Homebrew
         puts Formatter.columns(all_casks)
       end
 
-      if $stdout.tty? && !local_casks.include?(query)
+      if $stdout.tty?
         count = all_formulae.count + all_casks.count
 
-        if reason = MissingFormula.reason(query, silent: true)
+        if reason = MissingFormula.reason(query, silent: true) && !local_casks.include?(query)
           if count.positive?
             puts
             puts "If you meant #{query.inspect} specifically:"

commit e8cef45dffcd01c3aaa8c40be7abf8e0f3900a10

Fix that `reason` is coerced to a boolean
https://github.com/Homebrew/brew/commit/4d634c193a7e95b5344204119b9d1e11efff7f45 introduced a bug such that `reason` is coerced to a boolean, so when displayed it loses information about the actual return value from `MissingFormula.reason`. This patch re-scopes the assignment of `reason` such that the actual reason is retained, while also retaining the sense of the new boolean check.

Library/Homebrew/cmd/search.rb
@@ -103,7 +103,7 @@ module Homebrew
       if $stdout.tty?
         count = all_formulae.count + all_casks.count
 
-        if reason = MissingFormula.reason(query, silent: true) && !local_casks.include?(query)
+        if (reason = MissingFormula.reason(query, silent: true)) && !local_casks.include?(query)
           if count.positive?
             puts
             puts "If you meant #{query.inspect} specifically:"